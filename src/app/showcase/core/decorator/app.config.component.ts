24bb9eb80b98f60a1b9ae1d1f18e9a5d433bdfef
Waldemar Kłaczyński <wklaczynski>
2021-04-27 13:09:33 +0200

    Incubating system

---
+++ src/app/showcase/core/decorator/app.config.component.ts
@@ -0,0 +1,118 @@
+import {Component, ElementRef, OnInit, OnDestroy} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { AppConfigService } from '../service/appconfigservice';
+import { AppConfig } from '../domain/appconfig';
+import { Subscription } from 'rxjs';
+
+@Component({
+    selector: 'app-config',
+    templateUrl: './app.config.component.html',
+    styleUrls: ['./styles/decorator.scss'],
+})
+export class AppConfigComponent implements OnInit, OnDestroy {
+
+    active: boolean;
+
+    scale: number = 14;
+    scales: number[] = [12,13,14,15,16];
+
+    outsideClickListener: any;
+
+    config: AppConfig;
+
+    subscription: Subscription;
+
+    constructor(private el: ElementRef, private router: Router, private configService: AppConfigService) {}
+
+    ngOnInit() {
+        this.config = this.configService.config;
+        this.subscription = this.configService.configUpdate$.subscribe(config => {
+            this.config = config;
+            if (this.config.theme === 'nano')
+                this.scale = 12;
+            else
+                this.scale = 14;
+
+            this.applyScale();
+        });
+
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.active = false
+             }
+        });
+
+        if (this.config.theme === 'nano')
+            this.scale = 12;
+    }
+
+    toggleConfigurator(event: Event) {
+        this.active = !this.active;
+        event.preventDefault();
+
+        if (this.active)
+            this.bindOutsideClickListener();
+        else
+            this.unbindOutsideClickListener();
+    }
+
+    hideConfigurator(event) {
+        this.active = false;
+        this.unbindOutsideClickListener();
+        event.preventDefault();
+    }
+
+    changeTheme(event: Event, theme: string, dark: boolean) {
+        let themeElement = document.getElementById('theme-link');
+        themeElement.setAttribute('href', themeElement.getAttribute('href').replace(this.config.theme, theme));
+        this.configService.updateConfig({...this.config, ...{theme, dark}});
+        event.preventDefault();
+    }
+
+    onRippleChange() {
+        this.configService.updateConfig(this.config);
+    }
+
+    bindOutsideClickListener() {
+        if (!this.outsideClickListener) {
+            this.outsideClickListener = (event) => {
+                if (this.active && this.isOutsideClicked(event)) {
+                    this.active = false;
+                }
+            };
+            document.addEventListener('click', this.outsideClickListener);
+        }
+    }
+
+    unbindOutsideClickListener() {
+        if (this.outsideClickListener) {
+            document.removeEventListener('click', this.outsideClickListener);
+            this.outsideClickListener = null;
+        }
+    }
+
+    isOutsideClicked(event) {
+        return !(this.el.nativeElement.isSameNode(event.target) || this.el.nativeElement.contains(event.target));
+    }
+
+    decrementScale() {
+        this.scale--;
+        this.applyScale();
+    }
+
+    incrementScale() {
+        this.scale++;
+        this.applyScale();
+    }
+
+    applyScale() {
+        document.documentElement.style.fontSize = this.scale + 'px';
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
+}
--- src/app/showcase/core/decorator/decorator.module.ts
+++ src/app/showcase/core/decorator/decorator.module.ts
@@ -8,6 +8,7 @@ import { VersionService } from '../service/versionservice';
 import { AppNewsComponent } from './app.news.component';
 import { AppTopBarComponent } from './app.topbar.component';
 import { AppMenuComponent } from './app.menu.component';
+import { AppConfigComponent } from './app.config.component';
 import { AppFooterComponent } from './app.footer.component';
 
 @NgModule({
@@ -15,12 +16,14 @@ import { AppFooterComponent } from './app.footer.component';
     AppNewsComponent,
     AppTopBarComponent,
     AppMenuComponent,
+    AppConfigComponent,
     AppFooterComponent
   ],
   exports: [
     AppNewsComponent,
     AppTopBarComponent,
     AppMenuComponent,
+    AppConfigComponent,
     AppFooterComponent
   ],
   imports: [
